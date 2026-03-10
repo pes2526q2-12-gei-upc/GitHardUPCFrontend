835b2509e805767721aee1eb6d1465a0ae33d33a
Oscar <oscar.lopez.andreu@estudiantat.upc.edu>
2026-03-10 14:32:49 +0100

    TG[0]:config: Corregir Integració Continua

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("org.sonarqube") version "4.4.1.3373"
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 sonar {
