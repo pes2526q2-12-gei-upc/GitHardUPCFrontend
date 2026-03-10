8b477270b38f68592ed218957381434bfb3117ee
Oscar <oscar.lopez.andreu@estudiantat.upc.edu>
2026-03-10 14:24:07 +0100

    TG[0]:config: Corregir Integració Continua

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
@@ -7,15 +6,18 @@ plugins {
 
 sonar {
     properties {
-        property("sonar.projectKey", "GitHardUPCFrontend") // Consistente con el backend
+        property("sonar.projectKey", "GitHardUPCFrontend")
         property("sonar.projectName", "GitHardUPC Frontend")
-        property("sonar.host.url", "http://nattech.fib.upc.edu:40380") // Nuestra URL de Virtech
+        property("sonar.host.url", "http://nattech.fib.upc.edu:40380")
 
         // Rutas específicas de Android
         property("sonar.sources", "app/src/main/java")
         property("sonar.tests", "app/src/test/java, app/src/androidTest/java")
         property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
 
-        // Le decimos a Sonar dónde encontrará el XML de cobertura generado por JaCoCo
+        // Reporte de JaCoCo
         property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+
+        property("sonar.gradle.skipCompile", "true")
     }
+}
